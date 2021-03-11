989476aa81b51f9088e3630bbd88ff6553ed7b3c
Joe Elia <mrjoeelia@gmail.com>
2021-03-10 21:41:12 -0800

    commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,34 +1,44 @@
 <template>
-  <div class="h-full home">
-    <nfc-scan />
-  </div>
+    <div class="h-full home">
+      <fixed-header>
+        FSFSDFDS
+</fixed-header>
+      <h1
+        class="text-2xl font-bold leading-7 text-gray-900 sm:text-3xl sm:truncate"
+      >
+        Back End Developer
+      </h1>
+      <nfc-scan />
+    </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import NfcScan from "@/components/NFC/Scan.vue";
 import Haptic from "@/utils/Haptic";
-import { mapMutations } from 'vuex';
+import { mapMutations } from "vuex";
+import FixedHeader from '../components/Shared/FixedHeader.vue';
 export default {
   name: "Home",
   created() {
-    if (this.isViberate === 0){
+    if (this.isViberate === 0) {
       //first launch check
-       this.increment();
+      this.increment();
     } else {
-       Haptic.impactHeavy();
+      Haptic.impactHeavy();
     }
   },
   components: {
-    NfcScan
+    NfcScan,
+    FixedHeader
   },
-   methods: {
-    ...mapMutations('global', ['increment']),
-   },
+  methods: {
+    ...mapMutations("global", ["increment"])
+  },
   computed: {
-      isViberate () {
-        return this.$store.state.global.launched
-      },
+    isViberate() {
+      return this.$store.state.global.launched;
     }
+  }
 };
 </script>
